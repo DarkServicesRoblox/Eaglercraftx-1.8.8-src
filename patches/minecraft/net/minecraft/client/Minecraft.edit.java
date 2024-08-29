25a7a6380b7620004cea3aa2273232a1b7ac2277
DarkServicesRoblox <delsinwaleczny@gmail.com>
2024-08-30 00:42:32 +0200

    Update Minecraft.edit.java

--- patches/minecraft/net/minecraft/client/Minecraft.edit.java
+++ patches/minecraft/net/minecraft/client/Minecraft.edit.java
@@ -1,9 +1,9 @@
 
-# Eagler Context Redacted Diff
-# Copyright (c) 2023 lax1dude. All rights reserved.
+# BetterBuild Context Redacted Diff
+# Copyright (c) 2023 Floco. All rights reserved.
 
 # Version: 1.0
-# Author: lax1dude
+# Author: Floco
 
 > DELETE  2  @  2 : 17
 
